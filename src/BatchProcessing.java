533db7c68fca402b453d33057b742a4fd48b02ab
JJ <janarjanrb@gmail.com>
2025-06-12 23:02:44 +0545

    using prepared and statement only to add in db

---
+++ src/BatchProcessing.java
@@ -0,0 +1,8 @@
+//public class BatchProcessing {
+//    private static
+//    public static void main(String[] args) {
+//        try{
+//            Class.forName("com.mysql.jdbc.Driver")
+//        }
+//    }
+//}
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import com.mysql.cj.jdbc.ConnectionImpl;
 import com.mysql.cj.jdbc.Driver;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
     private static final String url = "jdbc:mysql://localhost:3306/mydb";
@@ -76,27 +77,74 @@ public class Main {
 //            if(returnedVal>0) System.out.println("Inserted Successfully!");
 //            else System.out.println("Insertion failed!");
 
-            // Trying to drop
+            // Trying to delete (we use drop to delete the table or database itself.)
+//            Connection connection = DriverManager.getConnection(url,username,password);
+//            PreparedStatement preparedStatement1 = connection.prepareStatement("DELETE FROM students WHERE id = ?");
+//            preparedStatement1.setInt(1,2);
+//            int dropVal = preparedStatement1.executeUpdate();
+//            if(dropVal > 0) System.out.println("Dropped successfully!");
+//            else System.out.println("Failed to drop");
+
+
+
+            // Batch processing
+
             Connection connection = DriverManager.getConnection(url,username,password);
-            PreparedStatement preparedStatement1 = connection.prepareStatement("DELETE FROM students WHERE id = ?");
-            preparedStatement1.setInt(1,2);
-            int dropVal = preparedStatement1.executeUpdate();
-            if(dropVal > 0) System.out.println("Dropped successfully!");
-            else System.out.println("Failed to drop");
+//            Statement statement = connection.createStatement();
+            String query = "INSERT INTO students(name, age, marks) VALUES(?,?,?)";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            Scanner sc = new Scanner(System.in);
+            while(true) {
+                System.out.print("Enter Name: ");
+                String name = sc.next();
+                System.out.print("Enter age: ");
+                int age = sc.nextInt();
+                System.out.print("Enter Marks: ");
+                double marks = sc.nextDouble();
+
+                sc.nextLine();
+                /*The "leftover" refers to the newline character (\n) that remains in the input buffer after using methods
+                like nextInt() or nextDouble() from Scanner. These methods read only the number, not the newline you press
+                after entering the value. When you call nextLine() next, it immediately reads this leftover newline, resulting
+                in skipped input.*/
+                preparedStatement.setString(1, name);
+                preparedStatement.setInt(2, age);
+                preparedStatement.setDouble(3, marks);
+                preparedStatement.addBatch();
+
+                System.out.print("\nAdd next? (Y/N): ");
+                String choice = sc.next().toUpperCase();
+//                String query = String.format("INSERT INTO students(name, age, marks) VALUES('%s',%d,%f)",name,age,marks);
+                if (choice.equals("N")) {
+                    break;
+                }
+            }
+                // So what happens in batch processing is when you execute the batch each statement is going to run independently meaning in the array
+                // each pocket or index '| |' is going to store the binary/bool value that is returned from the process success or failure
+                /* so if the process succeeds then 1 will be stored and vice versa like this --> |1|0|1|1|0|*/
+                int[] rowsAffected = preparedStatement.executeBatch();
+
+                for(int i = 0; i<rowsAffected.length; i++){
+                        if(rowsAffected[i] == 0){
+                            System.out.println(i+" query failed to execute.");
+                        }
+                    }
 
 
 
             //--------Viewing the contents of the table
-            String query = "SELECT * FROM students";
-            ResultSet resultSet = preparedStatement1.executeQuery(query);
+            String queryToView = "SELECT * FROM students";
+            ResultSet resultSet = preparedStatement.executeQuery(queryToView);
 
             while (resultSet.next())
             {
                 int id = resultSet.getInt("id");
                 String name = resultSet.getString("name");
                 double marks = resultSet.getDouble("marks");
+                int age = resultSet.getInt("age");
 
-                System.out.println("ID: " + id + "\nNAME: " + name + "\nMarks: "+ marks +"\n");
+                System.out.println("ID: " + id + "\nNAME: " + name + "\nAge: "+age+"\nMarks: "+ marks +"\n");
 
             }
         }catch (SQLException e){
